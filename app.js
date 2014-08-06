392113eeab0c22cda6ae5be80090db61427e999a
JiaHang Li <jiahang_li@outlook.com>
2014-08-06 22:58:01 +0800

    [Tak] An attempt of using node.js

---
+++ app.js
@@ -0,0 +1,13 @@
+var http = require('http');
+var fs = require('fs');
+var server = http.createServer();
+
+server.on('request', function(request, response){
+	response.writeHead(200);
+	var url = request.url;
+	url = (url == "/") ? "./index.html" : "." + url;
+	var prep = fs.createReadStream(url);
+	prep.pipe(response);
+}).listen(8000);
+
+console.log("server is running on 8000!");
